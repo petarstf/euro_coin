811e489f7b920dbaa5dbb2024870eda993328bd6
pier <petar.stf@gmail.com>
2020-07-24 13:22:48 +0200

    Updated stuff

--- routes/predict.js
+++ routes/predict.js
@@ -2,13 +2,26 @@ const express = require('express');
 const tf = require('@tensorflow/tfjs-node');
 const Jimp = require('jimp');
 const https = require('https');
+
 const fs = require('fs');
+
+async function cropImg(image) {
+  return Jimp.read('crop1.png').then(async img => {
+    await image.composite(img, 0, 0).write('cropped.png');
+    console.log('Cropped');
+    return true;
+  });
+}
 
 async function loadImg(buffer) {
   return Jimp.read(buffer).then(
-    (image) => {
+    async (image) => {
+      // await cropImg(image);
+      console.log('Uncropped');
+      // image.write('uncropped.png');
       const p = [];
       image.resize(150, 150);
+      // image.write('uncropped150.png');
 
       image.scan(0, 0, image.bitmap.width, image.bitmap.height, function test(
         x,
@@ -19,8 +32,7 @@ async function loadImg(buffer) {
         p.push(this.bitmap.data[idx + 1]/255.0);
         p.push(this.bitmap.data[idx + 2]/255.0);
       });
-      // p = p/255.0;
-      // console.log(p);
+
       return tf.tensor4d(p, [1, image.bitmap.width, image.bitmap.height, 3]);
     }
     ).catch(
@@ -30,94 +42,101 @@ async function loadImg(buffer) {
   )
 }
 
-async function predict(buffer) {
+async function checkIfCoin(buffer) {
   const input = await loadImg(buffer);
-  console.log(input);
-  predictions = await model.predict(input);
 
-  // console.log(predictions.dataSync());
+  predictions = await modelL1.predict(input);
+
+  var type = ['Not a coin', 'Coin'];
 
   const prediction = predictions
-      .reshape([743])
-      .argMax()
-      .dataSync()[0];
+        .flatten()
+        .dataSync()[0];
+  console.log('Type of input: ', type[prediction]);
+  return prediction;
+}
+
+async function checkWhichCoin(buffer) {
+  const input = await loadImg(buffer);
+  predictions = await modelL2.predict(input);
+  const prediction = predictions
+        .flatten()
+        .argMax()
+        .dataSync()[0];
   const accuracy = predictions
-      .reshape([743])
-      .max()
-      .dataSync()[0];
+        .flatten()
+        .max()
+        .dataSync()[0];
+  return { prediction, accuracy };
+}
+
+async function predCoin(buffer, model) {
+  const input = await loadImg(buffer);
+  predictions = await model.predict(input);
+
+  const prediction = predictions.flatten().argMax().dataSync()[0];
+  const accuracy = predictions.flatten().max().dataSync()[0];
+
   return { prediction: prediction, accuracy: accuracy };
 }
 
-async function writeOut(buffer) {
-  prediction = await predict(buffer);
-  console.log(prediction);
-  let index;
-  let side = 'obverse';
-  for(let i = 0; i < obverseCodes.length; i++) {
-    if(obverseCodes[i] == prediction.prediction) {
-      index = i;
-    }
-  }
-  for(let key in reverseCodes) {
-    if(reverseCodes[key] == prediction.prediction) {
-      index = key;
+async function writeOut(buffer, model, coinClass) {
+  prediction = await predCoin(buffer, model);
+
+  var side = 'obverse';
+  var newCoin;
+
+  coins.forEach(coin => {
+    if(coin.obvCodes == prediction.prediction && coin.class == coinClass) {
+      index = coin.index;
+      newCoin = coin;
+    } else if(coin.revCodes == prediction.prediction && coin.class == coinClass) {
+      index = coin.index;
       side = 'reverse';
+      newCoin = coin;
     }
-  }
-  return { index: index, side: side, accuracy: prediction.accuracy }
+  });
+
+  return { coin: newCoin, side: side, accuracy: (prediction.accuracy*100).toFixed(2) }
 }
 
-async function getCoin(buffer, res) {
-  item = await writeOut(buffer);
-  // console.log(item);
-  let api = "https://euro-coin-418c3.firebaseio.com/" + item.index + ".json";
-  https.get(api, 
-    response => {
-      response.setEncoding('utf8');
-      
-      let output = '';
-      response.on('data', (chunk) => {
-        output += chunk;
-      });
-      
-      response.on('end', () => {
-        let obj = JSON.parse(output);
-        let image = '';
-        if (item.side == "obverse") {
-          image = obj.obverse
-        } else {
-          image = obj.reverse;
-        }
-        response = {
-          index: obj.index,
-          name: obj.name,
-          year: obj.year,
-          image: image,
-          accuracy: item.accuracy,
-          country: obj.country,
-          desc: obj.desc,
-          specifications: {
-              diameter: obj.diameter,
-              material: obj.material,
-              obv_codes: obj.obv_codes,
-              rev_codes: obj.rev_codes,
-              shape: obj.shape,
-              thickness: obj.thickness,
-              alignment: obj.alignment,
-              weight: obj.weight,
-          },
-          price: obj.prices,
-        };
-        res.status(200).send(response);
-      });
-
-    }
-  );
+async function getCoin(buffer, model, coinClass, res) {
+  item = await writeOut(buffer, model, coinClass);
+  res.status(200).send(item);
 }
 
-const predictImage = (req,res,next) => {
+const predictImage = async (req,res,next) => {
   var buffer = Buffer.from(req.body.image, 'base64');
-  getCoin(buffer, res);
+
+  if(await checkIfCoin(buffer)) {
+    
+      var coinClass = await checkWhichCoin(buffer);
+      var classes = ['Other', '1 Euro', '2 Euro']
+      
+      console.log("Coin class: ", classes[coinClass.prediction], '\nAccuracy: ', (coinClass.accuracy*100).toFixed(2));
+
+      switch (coinClass.prediction) {
+        case 1:
+          // 1EuroModel
+          getCoin(buffer, model1e, coinClass.prediction, res);
+          break;
+        case 2:
+          // 2EuroModel
+          getCoin(buffer, model2e, coinClass.prediction, res);
+          break
+        case 0:
+          // OtherEuroModel
+          getCoin(buffer, modelOther, coinClass.prediction, res);
+          break;
+        default:
+          console.log("Something went wrong!");
+          break;
+      }
+
+  } else {
+    res.status(200).send({ accuracy: 0.00, message: 'Coin was not recognized' });
+  }
+
 };
 
 module.exports = predictImage;
